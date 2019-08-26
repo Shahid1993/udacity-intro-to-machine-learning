d12a6c30700d6883f1f0f0fe83e2c74fb461f44b
Shahid1993 <shahid.bhat1994@gmail.com>
2019-08-26 23:15:26 +0530

    Regression Completed

--- ud120-projects/regression/finance_regression.py
+++ ud120-projects/regression/finance_regression.py
@@ -17,7 +17,9 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+from sklearn.linear_model import LinearRegression
+
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified_unix.pkl", "rb") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
@@ -26,10 +28,10 @@ data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,8 +40,15 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+
+print('Coefficient:', reg.coef_)
+print('Intercept:',reg.intercept_)
 
 
+print('Training Score:',reg.score(feature_train, target_train))
+print('Testing Score:', reg.score(feature_test, target_test))
 
 
 
