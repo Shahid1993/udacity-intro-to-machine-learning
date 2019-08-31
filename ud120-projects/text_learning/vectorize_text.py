127e59f121c9027a483bd46e49ee109d05fbac48
Shahid1993 <shahid.bhat1994@gmail.com>
2019-08-31 19:21:08 +0530

    Text Learning Completed

--- ud120-projects/text_learning/vectorize_text.py
+++ ud120-projects/text_learning/vectorize_text.py
@@ -41,35 +41,56 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
+        #temp_counter += 1
+        #if temp_counter < 200:
+            # path = os.path.join('..', path[:-1])
+            path = r'../' +path[:-1]
+            print (path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            replaceWords = ["sara", "shackleton", "chris", "germani"]
+            for w in replaceWords:
+                text = text.replace(w, "")
 
             ### append the text to word_data
+            word_data.append(text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            from_data.append(0 if name == "sara" else 1)
 
             email.close()
 
-print "emails processed"
+print ("emails processed")
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
 
-
+# The string that you get for word_data[152]
+print("word_data[152]:",word_data[152])
 
 
 ### in Part 4, do TfIdf vectorization here
 
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer, CountVectorizer
+
+vectorizer = TfidfVectorizer(stop_words="english")
+X = vectorizer.fit_transform(word_data)
+
+transformer = TfidfTransformer()
+tfidf = transformer.fit_transform(X)
+
+vector = vectorizer.get_feature_names()
+
+# How many unique words are there in your Tfldf?
+print (len(vector))
+# What is word number 34597 in your TfIdf?
+print(vector[34597])
 
